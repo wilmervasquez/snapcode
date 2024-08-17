9f9cf495946632919a49309be8e1ecde86d89dfa
wilmervasquez2 <wilmerluisvasquezlino@gmail.com>
2024-08-17 01:50:47 -0500

    v0.1.0

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
+
 import { Draw } from "./lib/canvas.js";
 import {  fontFeatureSettings, img, months } from './lib/util.js'
 
 const snapCode = document.querySelector(".snap-code");
 const code = document.querySelector(".code");
 const canvas = document.querySelector("canvas");
-const cvW = document.querySelector(".canvas-width");
 
 // Inputs
+const app = {
+  by: document.getElementById("by"),
+  cvW: document.querySelector(".canvas-width"),
+  cvH: document.querySelector(".canvas-height")
+}
+
 const $title = document.getElementById("title");
-const $by = document.getElementById("by");
+
 const $lang = document.getElementById("languaje");
 const $background = document.getElementById("background");
 $background.value = localStorage.getItem('background') ?? ''
@@ -18,7 +24,6 @@ const $selectIconNetwork = document.getElementById("select-icon-network");
 const $selectFontFamily = document.querySelector("select");
 $selectIconNetwork.value = localStorage.getItem('iconNetwork') ?? 'instagram'
 
-const cvH = document.querySelector(".canvas-height");
 
 // CheckBoxs
 const $checkBoxItalic = document.querySelector(".italic");
@@ -26,7 +31,7 @@ const $ligatures = document.getElementById('font-ligatures')
 const $fondo = document.getElementById('fondo')
 
 const ctx = canvas.getContext("2d");
-const cv = new Draw(canvas,ctx)
+const drw = new Draw(canvas,ctx)
 const btnRemove = document.getElementById('btn-remove')
 
 $lang.value = localStorage.getItem('languaje') ?? "txt"
@@ -38,13 +43,19 @@ $lang.addEventListener('input', (e)=>{
 
 $fondo.addEventListener('change', draw)
 
+const codeSpace = {
+  linesInPlainText: []
+}
+
 
 let title = localStorage.getItem('title') ?? '';
 let by = localStorage.getItem('by') ?? "you"
 
-
+const config = {
+  borderRadius: 30
+}
 $title.value = title
-$by.value = by
+app.by.value = by
 let fontSize = 42
 let lineHeight = 1.4 * fontSize
 let fondo = true;
@@ -58,7 +69,7 @@ $ligatures.addEventListener('click', ()=> {
   draw()
 })
 
-let padding = fontSize;
+let padding = 30;
 let fontFamily = localStorage.getItem('fontFamily') ?? "Cascadia Code";
 $selectFontFamily.value = fontFamily
 let paddingLineNumbers = 50
@@ -73,35 +84,77 @@ const images = {
   iconNetwork: img(`icon/${ localStorage.getItem('iconNetwork') ?? 'instagram'}.svg`)
 }
 
-function draw() {
-  code.innerHTML = localStorage.getItem("html") ?? `<div style="color: #abb2bf;background-color: #282c34;font-family: Cascadia Code, MonoLisa, Consolas, 'Courier New', monospace;font-weight: normal;font-size: 14px;line-height: 19px;white-space: pre;"><div><span style="color: hsl(${Math.random()*360},100%,70%);font-style: italic;">Paste your code from VSCode</span></div></div>`;
-  
-  const bgColor = getComputedStyle(document.querySelector('code>div')).backgroundColor
-  console.log()
-  let isDark = bgColor.match(/\d+/g).reduce((ac, v)=>ac + Number(v),0) / 3 < 127;
 
-  console.log(isDark)
-  const rows = code.querySelectorAll("div > div, div > br");
-  const bgf = new Set()
+function loadStructCodeSpace() {
+  code.innerHTML = localStorage.getItem("html") ?? `<div style="color: #abb2bf;background-color: #282c34;font-family: Cascadia Code, MonoLisa, Consolas, 'Courier New', monospace;font-weight: normal;font-size: 14px;line-height: 19px;white-space: pre;"><div><span style="color: hsl(${Math.random()*360},100%,70%);font-style: italic;">Paste your code from VSCode</span></div></div>`;
+  codeSpace.bgColor = getComputedStyle(document.querySelector('code>div')).backgroundColor
+  codeSpace.isDark = codeSpace.bgColor.match(/\d+/g).reduce((ac, v)=>ac + Number(v),0) / 3 < 127;
+
+  let rows = []
+  let linesInPlainText = []
+  let $rows = code.querySelectorAll("div > div, div > br");
+
+  $rows.forEach(($line) => {
+    let line = []
+    linesInPlainText.push($line.innerText)
+    $line.childNodes.forEach(($token) => {
+      let $lineComputedStyle = getComputedStyle($token);
+      if ($token.innerText.startsWith('//')) {
+        line.push({
+          textContent: "↝ ",
+          fontStyle: 'normal',
+          fontWeight: $lineComputedStyle.fontWeight,
+          color: $lineComputedStyle.color,
+     
+
+        })
+        line.push({
+          textContent: $token.innerText.replace(/^\/\/ /,""),
+          fontStyle: 'normal',
+          fontWeight: $lineComputedStyle.fontWeight,
+          color: $lineComputedStyle.color,
+        })
+      } else {
+
+        line.push({
+          textContent: $token.innerText,
+          fontStyle: $lineComputedStyle.fontStyle,
+          fontWeight: $lineComputedStyle.fontWeight,
+          color: $lineComputedStyle.color,
+        })
+      }
+    })
+    rows.push(line)
+  });
+
+  codeSpace.rows = rows
+  codeSpace.linesInPlainText = linesInPlainText;
+}
+loadStructCodeSpace()
+
+function draw() {
+  const widthOfLines = new Set()
   let colsMax = new Set()
   
-  let heightCode = rows.length * lineHeight;
-  rows.forEach((row)=>{
-    ctx.font = `42px ${fontFamily}`;
-    bgf.add(ctx.measureText(row.innerText).width)
-    colsMax.add(row.innerText.length)
+  drw.setFont(fontSize, fontFamily)
+
+  let heightCode = codeSpace.rows.length * lineHeight;
+  codeSpace.linesInPlainText.forEach((textContent )=>{
+    widthOfLines.add(ctx.measureText(textContent).width)
+    colsMax.add(textContent.length)
   })
+
   colsMax = Math.max(...colsMax)
-  const bgfMax = Math.max(...bgf)
+  const maxWidthOfCodeSpace = Math.max(...widthOfLines)
   
-  ctx.font = `42px ${fontFamily}`;
-  let wLH = ctx.measureText(String(rows.length)).width
+  let wLH = ctx.measureText(String(codeSpace.rows.length)).width
   
   // 🫧 Canvas Dimensitions
-  canvas.width = padding*2 + (paddingLineNumbers*2 + wLH) * 2 + bgfMax;
+  canvas.width = padding *2 + (paddingLineNumbers*2 + wLH) * 2 + maxWidthOfCodeSpace;
   canvas.width = canvas.width < 1600 ? 1600 : canvas.width;
   canvas.height = (padding*2) + heightCode + 200;
   
+  drw.setFont(fontSize, fontFamily)
   // return
   if ($fondo.checked) {
     ctx.drawImage(
@@ -113,17 +166,15 @@ function draw() {
     )
   }
 
-  ctx.shadowColor = "rgba(0, 0, 0, .2)";
-  ctx.shadowBlur = padding;
-  ctx.shadowOffsetX = 0;
-  ctx.shadowOffsetY = 0;
-  ctx.fillStyle = bgColor;
-  cv.rectRound(
+  drw.setShadow("hsl(0,0%,0%, 0.3)", padding, 0 ,0)
+  console.dir(ctx)
+  ctx.fillStyle = codeSpace.bgColor;
+  drw.rectRound(
     padding,
     padding,
     canvas.width - padding * 2,
     canvas.height - padding * 2,
-    30
+    config.borderRadius
   ).fill();
   ctx.filter = 'none';
 
@@ -131,76 +182,85 @@ function draw() {
   let heightStatusBar = lineHeight*1.2
   ctx.shadowBlur = 0
   ctx.fillStyle = "rgba(0,0,0,.1)"
-  cv.fillRectRoundedBottom(
+  drw.fillRectRoundedBottom(
     padding, 
     canvas.height-padding-heightStatusBar, 
     canvas.width-padding*2, 
     heightStatusBar,
-    32
+    config.borderRadius + 2
   )
 
-  ctx.strokeStyle = isDark ? "rgba(255,255,255,.1)" : 'rgba(0,0,0,.2)';
+  ctx.strokeStyle = codeSpace.isDark ? "rgba(255,255,255,.1)" : 'rgba(0,0,0,.2)';
   ctx.lineWidth = 2;
-  cv.rectRound(
+  drw.rectRound(
     padding+1,
     padding+1,
     canvas.width - (padding * 2)-2,
     canvas.height - (padding * 2) -2,
-    32
+    config.borderRadius
   ).stroke();
 
   ctx.shadowBlur = 0;
 
   // circle
-  cv.circle(padding + 60, padding + 60, 16, "#F76452").fill();
-  cv.circle(padding + 120, padding + 60, 16, "#fdbf2c").fill();
-  cv.circle(padding + 180, padding + 60, 16, "#1ecf37").fill();
+  drw.circle(padding + 60, padding + 60, 16, "#F76452").fill();
+  drw.circle(padding + 120, padding + 60, 16, "#fdbf2c").fill();
+  drw.circle(padding + 180, padding + 60, 16, "#1ecf37").fill();
 
   const cod = { x: padding + (paddingLineNumbers*2)+wLH, y: padding + 120 };
 
 
   // 🫧 Icon Folder
   let size = 50
-  ctx.filter = isDark ? 'none' : 'invert(100)'
+  ctx.filter = codeSpace.isDark ? 'none' : 'invert(100)'
   ctx.drawImage(images.iconFolder, padding+240, padding+60-(size/2),size,size);
   ctx.filter = 'none'
 
   // 🫧 Title
   ctx.textBaseline = "middle";
-  ctx.font = `42px ${fontFamily}`;
   // ctx.fillStyle = `hsla(${Math.random()*360},100%,${isDark ? '90%,.7' : '10%,.7'})`
-  ctx.fillStyle = isDark ? `oklch(83% 0.1 ${Math.random()*360}deg / 0.5)` : '#00000099'
+  ctx.fillStyle = codeSpace.isDark ? `oklch(83% 0.1 ${Math.random()*360}deg)` : '#00000099'
   ctx.fillText(title, padding + 310, padding+60)
 
-  ctx.filter = isDark ? 'none' : 'invert(100)'
+  ctx.filter = codeSpace.isDark ? 'none' : 'invert(100)'
   ctx.drawImage(images.iconCube, canvas.width-padding-90, padding+60-(size/2),size,size);
   ctx.drawImage(images.iconPlay, canvas.width-padding-170, padding+60-(size/2),size,size);
   ctx.filter = 'none'
 
   ctx.textBaseline = "top";
-  rows.forEach((row, i) => {
+  codeSpace.rows.forEach((line, i) => {
     
+    // resaltado
+    // let rest = lineHeight/6
+    // if (i == 5 || i == 8) {
+    //   ctx.fillStyle = 'hsla(135,100%,70%,0.1)'
+    //   ctx.fillRect(padding, cod.y + (i*lineHeight)-rest, canvas.width - (padding * 2), lineHeight)
+    //   ctx.fillStyle = 'hsla(135,100%,70%,0.5)'
+    //   ctx.fillRect(padding, cod.y + (i*lineHeight)-rest, 6, lineHeight)
+    // }
+    // if (i == 16 || i == 7) {
+    //   ctx.fillStyle = 'hsla(5,100%,70%,0.1)'
+    //   ctx.fillRect(padding, cod.y + (i*lineHeight)-rest, canvas.width - (padding * 2), lineHeight)
+    //   ctx.fillStyle = 'hsla(5,100%,70%,0.8)'
+    //   ctx.fillRect(padding, cod.y + (i*lineHeight)-rest, 6, lineHeight)
+    // }
+
     // line Numbers
     ctx.textAlign = "right";
-    ctx.fillStyle = isDark ? 'rgba(255,255,255,.2)' : 'rgba(0,0,0,.4)';
-    ctx.font = `42px ${fontFamily}`;
+    ctx.fillStyle = codeSpace.isDark ? 'rgba(255,255,255,.3)' : 'rgba(0,0,0,.4)';
+    drw.setFont(fontSize, fontFamily);
     ctx.fillText(
       i+1,
       padding + paddingLineNumbers + wLH,
       i*lineHeight+cod.y
     );
 
-    
-   
-
-    row = row.querySelectorAll("span");
-
     ctx.textAlign = "left";
 
     // ✏️ Indentation
-    if (row[0] !== undefined && /^(\s\s)+/.test(row[0].textContent)) {
+    if (line[0] !== undefined && /^(\s\s)+/.test(line[0].textContent)) {
       let r = ctx.measureText("..").width;
-      let text = [...row[0].textContent.replaceAll(/\s/g,' ')];
+      let text = [...line[0].textContent.replaceAll(/\s/g,' ')];
       // let gj = text.match(/\s/g);
       ctx.fillStyle = "rgba(255,255,255,.1)";
 
@@ -219,47 +279,47 @@ function draw() {
     }
 
     if(Math.random() >= 0.5) {
-      ctx.fillStyle="rgb(255,255,255,0.1)"
-      cv.rectRound(
+      ctx.fillStyle = `hsla(${Math.random()*360},100%,90%,0.1)`
+      drw.rectRound(
         canvas.width-padding-20,
-        cod.y + (i * lineHeight) + (lineHeight/4) ,
+        cod.y + (i * lineHeight)  -3,
         10,
-        lineHeight*0.5,
+        lineHeight*0.75,
         5
       ).fill();
     }
 
     let left = cod.x
-    row.forEach((span, i2) => {
+    line.forEach(({ textContent, fontStyle, fontWeight, color, shadowColor }, i2) => {
       ctx.textAlign = "left";
 
-      let fg = getComputedStyle(span);
-
+      
+ 
       if ($checkBoxItalic.checked) {
         
-        ctx.font = `${fg.fontWeight} ${fg.fontStyle} 42px ${fontFamily}`;
+        drw.setFont(fontSize,fontFamily, fontWeight, fontStyle);
       } else {
-
-        ctx.font = `${fg.fontWeight} 42px ${fontFamily}`;
+        drw.setFont(fontSize,fontFamily, fg.fontWeight);
       }
 
-      ctx.fillStyle = "rgba(255,255,255,.03)";
       
       let x = left 
       let y = cod.y + (i * lineHeight) 
-      ctx.fillText(
-        span.textContent.replaceAll(/\s/g, "•"),
-        x,
-        y
-      );
       
-      ctx.fillStyle = fg.color;
-      ctx.fillText(
-        span.textContent,
-        x,
-        y
-      );
-      left += ctx.measureText(span.textContent).width 
+      
+      ctx.fillStyle = "rgba(255,255,255,.03)";
+
+      let text = textContent.replace(/\s/g, " ");
+
+      let txtM = text.replace(/\s/g, "•")
+      ctx.fillText( txtM, x, y );
+      
+      if (shadowColor) drw.setShadow(shadowColor, 14, 0 ,0)
+      ctx.fillStyle = color;
+      ctx.fillText(text, x, y);
+      drw.setShadow(shadowColor, 0, 0 ,0)
+
+      left += ctx.measureText(text).width 
     });
   });
 
@@ -268,14 +328,14 @@ function draw() {
 
   // 🫧 Bar Status Info
   ctx.textBaseline = 'middle'
-  ctx.fillStyle = isDark ? "rgb(255,255,255)" : "rgb(0,0,0)"
-  ctx.font = '38px DM Sans'
+  ctx.fillStyle = codeSpace.isDark ? "rgb(255,255,255)" : "rgb(0,0,0)"
+  drw.setFont(36, 'DM Sans')
   ctx.globalAlpha = 0.4
 
   let today = new Date() 
-  ctx.fillText(`∿ ${months[today.getMonth()]} ${today.getDate()}, ${today.getFullYear()}    Ln ${rows.length}, Col ${colsMax}`, padding + 30, canvas.height - padding-heightStatusBar/2)
+  ctx.fillText(`∿ ${months[today.getMonth()]} ${today.getDate()}, ${today.getFullYear()}    Ln ${codeSpace.rows.length}, Col ${colsMax}`, padding + 30, canvas.height - padding-heightStatusBar/2)
 
-  ctx.filter = isDark ? 'none' : 'invert(100)'
+  ctx.filter = codeSpace.isDark ? 'none' : 'invert(100)'
   ctx.drawImage(
     images.iconNetwork, 
     canvas.width - padding - paddingLineNumbers-ctx.measureText(by).width-ctx.measureText(languaje).width-145, 
@@ -302,10 +362,8 @@ function draw() {
   ctx.globalAlpha = 1
 
   // 👁️ Show Info Canvas
-  cvW.textContent = "Width: " + canvas.width
-  cvH.textContent = "Height: " + canvas.height
-
-  localStorage.setItem("html", code.innerHTML)
+  app.cvW.textContent = "Width: " + canvas.width
+  app.cvH.textContent = "Height: " + canvas.height
 }
 
 const btnPaste = document.querySelector(".btn-paste");
@@ -317,6 +375,7 @@ btnPaste.addEventListener("click", async () => {
       const htmlBlob = await clipboardItem.getType("text/html");
       const htmlText = await htmlBlob.text();
       localStorage.setItem('html', htmlText)
+      loadStructCodeSpace()
     }
   }
 
@@ -334,13 +393,12 @@ $title.addEventListener('input', (e)=>{
   localStorage.setItem('title',title)
   draw()
 })
-$by.addEventListener('input', (e)=>{
+app.by.addEventListener('input', (e)=>{
   by = e.target.value
   localStorage.setItem('by',by)
   draw()
 });
 $background.addEventListener('keyup', (e)=>{
-  console.log(e)
   if(e.key === 'Enter'){ 
     localStorage.setItem('background', e.target.value)
     images.background.src = e.target.value
@@ -360,12 +418,6 @@ btnRemove.addEventListener('click', () => {
   localStorage.removeItem('html')
   draw()
 })
-
-
-
-
-
-
 
 
 
